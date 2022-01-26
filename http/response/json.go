4b86507789da3adac51f180a37986b75ec99e07c
danisbagus <danisbagussetiawan@gmail.com>
2022-01-26 23:21:52 +0700

    add error function on http response package

--- http/response/json.go
+++ http/response/json.go
@@ -7,10 +7,23 @@ import (
 
 const JsonContentType = "application/json"
 
+type ErrorData struct {
+	ErrorID int    `json:"error_id"`
+	Message string `json:"message"`
+}
+
 func Write(w http.ResponseWriter, code int, data interface{}) {
 	w.Header().Add("Content-Type", JsonContentType)
 	w.WriteHeader(code)
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		panic(err)
 	}
+}
+
+func Error(w http.ResponseWriter, code int, message string) {
+	w.Header().Add("Content-Type", JsonContentType)
+	w.WriteHeader(code)
+
+	errorData := ErrorData{ErrorID: code, Message: message}
+	Write(w, code, errorData)
 }
